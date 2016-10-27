79e81d89737e15798d1881c9bea12f1af042b0d6
ClementeDR <dirosaclemente96@gmail.com>
2016-10-27 14:35:00 +0200

    Esercizi completati

--- Esercizi-C-26-10-2016/main.c
+++ Esercizi-C-26-10-2016/main.c
@@ -29,6 +29,7 @@ float floatNumbers[] = {35.4, 46.7, 77.55, 11.1, 9.04, 0.75 };
 int main(){
     int dim = 10;
     int numbers[dim];
+    int command;
 
     int i;
     //riempio l'array con valori da 1-10 ma mi scoccio di scriverli a mano
@@ -36,23 +37,74 @@ int main(){
         numbers[i] = i + 1;
     }
 
-    esercizio1(numbers, dim);
+    do{
+        printf("Inserisci il comando per: \n");
+        printf("1) Per esercizio 1 \n");
+        printf("2) Per esercizio 2 \n");
+        printf("3) Per esercizio 3 \n");
+        printf("4) Per esercizio 4 \n");
+        printf("5) Per esercizio 5 \n");
+        printf("6) Per esercizio 6 \n");
+        printf("7) Per esercizio 7 \n");
+        printf("8) Per esercizio 8 \n");
+        printf("9) Per esercizio 9 \n");
+        printf("0) Per Uscire \n");
 
-    esercizio2(numbers, dim);
+        scanf("%i", &command);
+        switch (command){
+            case 1:
+                printf("Esercizio %d \n", command);
+                esercizio1(numbers, dim);
+                break;
+            case 2:
+                printf("Esercizio %d \n", command);
+                esercizio2(numbers, dim);
+                break;
+            case 3:
+                printf("Esercizio %d \n", command);
+                esercizio3(numbers, dim);
+                break;
+            case 4:
+                printf("Esercizio %d \n", command);
+                esercizio4();
+                break;
+            case 5:
+                printf("Esercizio %d \n", command);
+                esercizio5();
+                break;
+            case 6:
+                printf("Esercizio %d \n", command);
+                esercizio6();
+                break;
+            case 7:
+                printf("Esercizio %d \n", command);
+                esercizio7();
+                break;
+            case 8:
+                printf("Esercizio %d \n", command);
+                esercizio8();
+                break;
+            case 9:
+                printf("Esercizio %d \n", command);
+                esercizio9();
+                break;
+            case 0:
+                printf("Alla prossima \n");
+                break;
 
-    esercizio3(numbers, dim);
+            case 69:
+                printf("Porcellino \n");
+                break;
 
-    esercizio4();
+            default:
+                printf("Bischero -.- \n");
+                break;
 
-    esercizio5();
+        }
 
-    esercizio6();
 
-    esercizio7();
-
-    esercizio8();
-
-    esercizio9();
+        printf("---------------------\n");
+    }while(command != 0);
 
 
     return 0;
